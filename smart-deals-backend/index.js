89b1eef0da727fa5076b0f58c0aef31019a7edc2
mehrab-abir <mehrababir100@gmail.com>
2025-12-15 12:51:17 -0500

    env fixed

--- smart-deals-backend/index.js
+++ smart-deals-backend/index.js
@@ -6,7 +6,8 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const admin = require("firebase-admin");
 const port = process.env.PORT || 3000;
 
-const serviceAccount = require("./smart-deals-firebase-admin-key.json");
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
